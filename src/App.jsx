938430a3df37f9c5c21dcb167ba4ef78cef2d62b
Anirban Dutta <duttashaan102@gmail.com>
2024-03-18 02:23:03 +0530

    Added all necessary frontend files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Spinner from "./components/Spinner/Spinner";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
+import Account from "./components/Account/Account";
+
+import { auth, getUserFromDatabase } from "./firebase";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userDetails, setUserDetails] = useState({});
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
+
+  const fetchUserDetails = async (uid) => {
+    const userDetails = await getUserFromDatabase(uid);
+    setUserDetails(userDetails);
+    setIsDataLoaded(true);
+  };
+
+  useEffect(() => {
+    const listener = auth.onAuthStateChanged((user) => {
+      if (!user) {
+        setIsDataLoaded(true);
+        setIsAuthenticated(false);
+        return;
+      }
+
+      setIsAuthenticated(true);
+      fetchUserDetails(user.uid);
+    });
+
+    return () => listener();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Router>
+        {isDataLoaded ? (
+          <Routes>
+            {!isAuthenticated && (
+              <>
+                <Route path="/login" element={<Auth />} />
+                <Route path="/signup" element={<Auth signup />} />
+              </>
+            )}
+            <Route
+              path="/account"
+              element={
+                <Account userDetails={userDetails} auth={isAuthenticated} />
+              }
+            />
+            <Route path="/" element={<Home auth={isAuthenticated} />} />
+            <Route path="/*" element={<Navigate to="/" />} />
+          </Routes>
+        ) : (
+          <div className="spinner">
+            <Spinner />
+          </div>
+        )}
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
